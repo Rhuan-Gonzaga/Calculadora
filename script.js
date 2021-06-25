f27d07ad67a033f2579fb98fa8ad020c0aa6065f
Indio santos <rhuangonzaga22@gmail.com>
2021-06-25 11:59:29 -0300

    finalizado

---
+++ script.js
@@ -0,0 +1,38 @@
+var form = document.getElementById("formulario");
+var resultado = document.getElementById("resultado");
+var tipo = document.getElementById("tipo");
+
+form.addEventListener("submit",function raiz(previneSub){
+    let n1 = parseInt(numero1.value);
+    let n2 = parseInt(numero2.value);
+    let tp = tipo.value;
+    
+
+    switch(tp){
+        case "+":
+            let total = n1+n2
+            resultado.innerHTML = n1 +" "+tp+" "+ n2 +" = "+total;
+    }
+
+    switch(tp){
+        case "-":
+            let total = n1-n2
+            resultado.innerHTML = n1 +" "+tp+" "+ n2 +" = "+total;
+    }
+
+    switch(tp){
+        case "x":
+            let total = n1*n2
+            resultado.innerHTML = n1 +" "+tp+" "+ n2 +" = "+total;
+    }
+
+    switch(tp){
+        case "/":
+            let total = n1/n2
+            resultado.innerHTML = n1 +" "+tp+" "+ n2 +" = "+total;
+    }
+
+
+    previneSub.preventDefault();
+    
+});
